896637c802e0bb4ad91fd5b77d1875b5f9d18a07
shohan11d <shohan11d@gmail.com>
2026-05-24 16:30:55 +0600

    52-5 Destination Details Page

---
+++ src/app/destinations/[id]/page.jsx
@@ -0,0 +1,37 @@
+import Image from "next/image";
+import { FaRegCalendar } from "react-icons/fa";
+import { LuMapPin } from "react-icons/lu";
+
+export default async function DestinationDetailsPage({ params }) {
+  const { id } = await params;
+  console.log(id);
+
+  const res = await fetch(`http://localhost:5000/destination/${id}`);
+  const destination = await res.json();
+
+  console.log(destination);
+  const { destinationName, price, duration, country, imageUrl, description } = destination;
+
+  return (
+    <div className="max-w-7xl mx-auto">
+      <Image src={imageUrl} width={800} height={500} alt={destinationName} className="w-full h-100 object-cover" />
+      <div className="p-2">
+        <div className="flex items-center gap-2">
+          <LuMapPin /> <span>{country}</span>
+        </div>
+        <div className="flex justify-between">
+          <h2 className="text-xl font-bold">{destinationName}</h2>
+        </div>
+
+        <div className="flex items-center gap-1">
+          <FaRegCalendar /> {duration}
+        </div>
+        <div className="text-2xl font-bold">
+          <h3>{price}</h3>
+        </div>
+        <h1 className="mt-10 text-2xl font-bold">Overview</h1>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+}
--- src/components/DestinationCard.jsx
+++ src/components/DestinationCard.jsx
@@ -1,9 +1,13 @@
+import { Button } from "@heroui/react";
 import Image from "next/image";
+import Link from "next/link";
 import { FaRegCalendar } from "react-icons/fa";
+import { FiExternalLink } from "react-icons/fi";
 import { LuMapPin } from "react-icons/lu";
 
 export default function DestinationCard({ destination }) {
   const {
+    _id,
     imageUrl,
     destinationName,
     country,
@@ -14,13 +18,15 @@ export default function DestinationCard({ destination }) {
   } = destination;
   return (
     <div className="border rounded-lg">
-      <Image
-        src={imageUrl}
-        alt={destinationName}
-        width={400}
-        height={300}
-        className="rounded-lg object-cover"
-      />
+      <div className="h-40 overflow-hidden">
+        <Image
+          src={imageUrl}
+          alt={destinationName}
+          width={400}
+          height={400}
+          className="rounded-lg h-full w-full object-cover"
+        />
+      </div>
       <div className="p-2">
         <div className="flex items-center gap-2">
           <LuMapPin /> <span>{country}</span>
@@ -32,13 +38,18 @@ export default function DestinationCard({ destination }) {
         <div className="flex items-center gap-1">
           <FaRegCalendar /> {duration}
         </div>
+        <div className="text-2xl font-bold">
+          <h3>{price}</h3>
+        </div>
       </div>
 
-      <div className="text-2xl font-bold">
-        {" "}
-        <h3>{price}</h3>
-      </div>
-    
+      <Link
+        href={`/destinations/${_id}`}
+        variant="ghost"
+        className={`mt-1 text-cyan-500`}
+      >
+        <FiExternalLink /> Book Now
+      </Link>
     </div>
   );
 }
